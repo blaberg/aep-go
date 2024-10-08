7ae30ba8464b40b5a4b06157a897c59cdad441b7
John Blåberg Kristoffersson <john.kristoffersson@einride.tech>
2024-10-08 15:42:51 +0200

    feat(resourcepath): re-implement resource path package

--- resourcepath/resourcepath.go
+++ resourcepath/resourcepath.go
@@ -2,38 +2,54 @@ package resourcepath
 
 import (
 	"fmt"
-	"reflect"
-	"strings"
+	"io"
 )
 
-func UnmarshalString(path string, s any) error {
-	v := reflect.ValueOf(s)
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
+// todo: det slutgiliga målet är att vi vill kunna generara rsurrser som använder den här funktionen.
+// De ska genereras som {message}ResourcePath. Jag vill att de ska ha GetOrganization() funktioner som returnerar strings
+// för varje segmnet av resurs namnet. Tror det blir en snyggare lösning än det vi har nu
+// alla validate och hasWilcard functioner ska ckså finnas på det
+// generellt tror jag vi kan göra paketet mycket simplare
+
+type ResourcePath struct {
+	segments map[string]string
+}
+
+func (p ResourcePath) Get(segment string) string {
+	return p.segments[segment]
+}
+
+func ParseString(path, pattern string) (*ResourcePath, error) {
+	if pattern == "" {
+		return nil, fmt.Errorf("pattern can't be empty")
 	}
-	t := v.Type()
-	st := reflect.TypeOf(v)
-	pattern := ""
-OUTER:
-	for i := range st.NumField() {
-		field := t.Field(i)
-		tag := field.Tag.Get("path")
-		if tag == "" {
+	segments := make(map[string]string)
+	var pathScanner, patternScanner Scanner
+	patternScanner.Init(pattern)
+	pathScanner.Init(path)
+	for patternScanner.Scan() {
+		if patternScanner.Full() {
+			return nil, fmt.Errorf("invalid pattern")
+		}
+		if !pathScanner.Scan() {
+			return nil, fmt.Errorf("segment %s: %w", patternScanner.Segment(), io.ErrUnexpectedEOF)
+		}
+		pathSegment, patternSegment := pathScanner.Segment(), patternScanner.Segment()
+		if !patternSegment.IsVariable() {
+			if patternSegment.Literal() != pathSegment.Literal() {
+				return nil, fmt.Errorf("segment %s: got %s", patternSegment, pathSegment)
+			}
+			if patternSegment.Literal() == Wildcard {
+				return nil, fmt.Errorf("the pattern can't contain wildcards")
+			}
 			continue
 		}
-		ps := strings.Split(tag, ",")
-		for _, p := range ps {
-			if Match(p, path) {
-				pattern = p
-				break OUTER
-			}
-		}
+		segments[patternSegment.Literal().ResourceID()] = pathSegment.Literal().ResourceID()
 	}
-	if pattern == "" {
-		return fmt.Errorf("no matching patterns for path: %s", path)
+	if pathScanner.Scan() {
+		return nil, fmt.Errorf("got trailing segments in path")
 	}
-	if err := Sscan(path, pattern, v); err != nil {
-		return err
-	}
-	return nil
+	return &ResourcePath{
+		segments: segments,
+	}, nil
 }
--- resourcepath/resourcepath_test.go
+++ resourcepath/resourcepath_test.go
@@ -3,58 +3,89 @@ package resourcepath
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/protobuf/testing/protocmp"
 	"gotest.tools/v3/assert"
 )
 
-type UserPathType string
-
-type UserPath struct {
-	Groups       string `path:"groups/{group}/users/{user}"`
-	Organization string `path:"organizations/{organization}/users/{user},organizations/{organization}/groups/{group}/users/{user}"`
-	User         string `path:"users/{user}"`
-}
+const (
+	orgPattern  = "organizations/{organization}"
+	userPattern = "organizations/{organization}/users/{user}"
+	singleton   = orgPattern + "/logs"
+)
 
 func Test_ResourcePath(t *testing.T) {
 	for _, tt := range []struct {
-		name string
-		path string
-		resp UserPath
-		err  string
+		name    string
+		path    string
+		pattern string
+		resp    *ResourcePath
+		err     string
 	}{
 		{
-			name: "valid",
-			path: "users/test-user",
-			resp: UserPath{
-				User: "test-user",
+			name:    "valid organization path",
+			path:    "organizations/test-org",
+			pattern: orgPattern,
+			resp: &ResourcePath{
+				segments: map[string]string{
+					"organization": "test-org",
+				},
 			},
 		},
 		{
-			name: "valid",
-			path: "organizations/test-org/users/test-user",
-			resp: UserPath{
-				User:         "test-user",
-				Organization: "test-org",
+			name:    "valid",
+			path:    "organizations/test-org/users/test-user",
+			pattern: userPattern,
+			resp: &ResourcePath{
+				segments: map[string]string{
+					"user":         "test-user",
+					"organization": "test-org",
+				},
 			},
 		},
 		{
-			name: "bad",
-			path: "organizatios/test-org/users/test-user",
-			err:  "no matching patterns for path: organizatios/test-org/users/test-user",
+			name:    "valid singleton",
+			path:    "organizations/test-org/logs",
+			pattern: singleton,
+			resp: &ResourcePath{
+				segments: map[string]string{
+					"organization": "test-org",
+				},
+			},
 		},
 		{
-			name: "bad",
-			path: "groups/test-org/users/test-user",
-			err:  "parse resource path 'groups/test-org/users/test-user' with pattern 'groups/{group}/users/{user}': failed to unmarshal path",
+			name:    "invalida pattern",
+			pattern: userPattern,
+			path:    "organizations/test-org",
+			err:     "segment users: unexpected EOF",
+		},
+		{
+			name: "empty pattern",
+			path: "organizations/test-org",
+			err:  "pattern can't be empty",
+		},
+		{
+			name:    "empty path",
+			pattern: userPattern,
+			err:     "segment organizations: unexpected EOF",
+		},
+		{
+			name: "empty pattern",
+			path: "organizations/test-org",
+			err:  "pattern can't be empty",
+		},
+		{
+			name: "empty pattern",
+			path: "organizations/test-org",
+			err:  "pattern can't be empty",
 		},
 	} {
-		var path UserPath
-		err := UnmarshalString(tt.path, &path)
+		path, err := ParseString(tt.path, tt.pattern)
 		if tt.err != "" {
 			assert.Error(t, err, tt.err)
 		} else {
 			assert.NilError(t, err)
-			assert.DeepEqual(t, path, tt.resp, protocmp.Transform())
+			assert.DeepEqual(t, path, tt.resp, protocmp.Transform(), cmp.AllowUnexported(ResourcePath{}))
 		}
 	}
 }
