2bf053ff1557a4d2a459f977dc9dc0c55d6a7bbc
John Blåberg Kristoffersson <john.kristoffersson@einride.tech>
2024-10-14 20:14:47 +0200

    fix: shorten base32 encoding alphabet

--- resourceid/generate.go
+++ resourceid/generate.go
@@ -6,7 +6,7 @@ import (
 	"github.com/google/uuid"
 )
 
-var encoder = base32.NewEncoding("abcdefghijklmnopqrstuvwxyz0123456789").WithPadding(base32.NoPadding)
+var encoder = base32.NewEncoding("abcdefghijklmnopqrstuvwxyz234567").WithPadding(base32.NoPadding)
 
 func New() string {
 	id := uuid.New()
